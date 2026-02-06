eaf7760535cc73938acf19bbcf99da1a00eed312
Blackentiety <lukas.boudinl@gmail.com>
2026-02-06 10:54:59 +0100

    refactor construct for attribut tracks + addTrack method + getTrack method + testAlbumTracks passed

--- src/Model/Album.php
+++ src/Model/Album.php
@@ -4,11 +4,13 @@ final class Album {
     private string $title;
     private int $year;
     private Artist $artist;
+    private array $tracks;
     public function __construct(string $title, int $year, Artist $artist) {
         $this->title = $title;
         $this->year = $year;
         $this->artist = $artist;
         $artist->addAlbum($this);
+        $this->tracks = [];
     }
     public function getTitle(): string {
         return $this->title;
@@ -18,5 +20,11 @@ final class Album {
     }
     public function getArtist(): Artist {
         return $this->artist;
+    }
+    public function addTrack(Track $track): void {
+        $this->tracks[] = $track;
+    }
+    public function getTracks(): array {
+        return $this->tracks;
     }
 }
