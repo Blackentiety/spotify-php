fb9342c156b3b562c70b4fb85364687ec44962b8
Blackentiety <lukas.boudinl@gmail.com>
2026-02-06 11:46:47 +0100

    class Library + addTrack method

--- src/Model/Library.php
+++ src/Model/Library.php
@@ -1,1 +1,21 @@
 <?php
+namespace Spotify\Model;
+class Library {
+    private array $tracks=[];
+    private array $albums=[];
+    private array $playlists=[];
+    private array $artists=[];
+    public function addTrack(Track $track): void {
+        $artist = $track->getArtist();
+        $album = $track->getAlbum();
+        if (!in_array($track, $this->tracks)) {
+            $this->tracks[] = $track;
+            if (!in_array($album, $this->albums)) {
+                $this->albums[] = $album;
+            }
+            if (!in_array($artist, $this->artists)) {
+                $this->artists[] = $artist;
+            }
+        }
+    }
+}
