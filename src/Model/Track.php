aa18d364065daab17363056ce759305428ffc5c3
Blackentiety <lukas.boudinl@gmail.com>
2026-02-06 10:05:33 +0100

    class Track arguments and construct

--- src/Model/Track.php
+++ src/Model/Track.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace Spotify\Model;
+
+class Track {
+    private string $title;
+    private int $length;
+    private int $rating;
+    private Album $album;
+    public function __construct(string $title, int $length) {
+        $this->title = $title;
+        $this->length = $length;
+        $this->rating = 0;
+    }
+}
