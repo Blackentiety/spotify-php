65ffa3a5a0659e38d05d0dd29c4ac85d39acfaa6
Blackentiety <lukas.boudinl@gmail.com>
2026-02-06 10:52:05 +0100

    class Album attribut and construct

--- src/Model/Album.php
+++ src/Model/Album.php
@@ -1,1 +1,13 @@
 <?php
+namespace Spotify\Model;
+final class Album {
+    private string $title;
+    private int $year;
+    private Artist $artist;
+    public function __construct(string $title, int $year, Artist $artist) {
+        $this->title = $title;
+        $this->year = $year;
+        $this->artist = $artist;
+        $artist->addAlbum($this);
+    }
+}
