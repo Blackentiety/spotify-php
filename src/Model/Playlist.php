83a18c5db43898e1ac00ef392d6deea922ce6541
Blackentiety <lukas.boudinl@gmail.com>
2026-02-06 11:15:05 +0100

    class Playlist and construct method

--- src/Model/Playlist.php
+++ src/Model/Playlist.php
@@ -1,1 +1,10 @@
 <?php
+namespace Spotify\Model;
+class Playlist {
+    private string $title;
+    private array $tracks;
+    public function __construct(string $title) {
+        $this->title = $title;
+        $this->tracks = [];
+    }
+}
